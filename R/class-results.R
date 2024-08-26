034df2e70c2d9456717bbf140d570c1eea81adef
DISOhda <diso.fbmn@h-da.de>
2024-08-26 16:21:45 +0200

    Example: correction and minor improvement

--- R/class-results.R
+++ R/class-results.R
@@ -49,9 +49,9 @@
 #'   # numerical vector of p-values
 #'   p_values = pv,
 #'   # list of supports (here: only one support); values must be sorted and unique
-#'   pvalue_supports = list(sort(unique(support))),
+#'   pvalue_supports = list(unique(sort(support))),
 #'   # list of indices that indicate which p-value/hypothesis each support belongs to
-#'   support_indices = list(1:m),
+#'   support_indices = as.list(1:m),
 #'   # name of input data variables
 #'   data_name = "x, n and p"
 #' )
