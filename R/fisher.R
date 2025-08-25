f25f9faf65671c08b1c00cb9e3a7d0fb65b48c9a
DISOhda <diso.fbmn@h-da.de>
2025-08-25 11:43:29 +0200

    Set minimum demanded number of values for checks

--- R/fisher.R
+++ R/fisher.R
@@ -117,7 +117,7 @@ fisher_test_pv <- function(
     if(any(dim(x) != c(2, 2)) && ncol(x) != 4 && nrow(x) != 4)
       cli_abort(error_msg_x)
     # check if all values are non-negative and close to integer
-    assert_integerish(x, lower = 0)
+    assert_integerish(x, lower = 0, min.len = 2)
     # round to integer
     x <- round(x)
     mode(x) <- "integer"
