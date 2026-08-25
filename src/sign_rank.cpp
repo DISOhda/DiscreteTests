1b35630d756bbabcb4faf522216eeb3a6137193f
DISOhda <diso.fbmn@h-da.de>
2026-08-25 11:50:10 +0200

    Switch to safe integer types for Mann-Whitney and Wilcoxon distribution computation

--- src/sign_rank.cpp
+++ src/sign_rank.cpp
@@ -7,17 +7,17 @@ List sign_rank_probs_int(
     const NumericVector n  // must be sorted!!
 ) {
   // number of distributions to be computed
-  int len = n.length();
+  int32_t len = n.length();
   // largest sample size
-  int max_n = n[len - 1];
+  int32_t max_n = n[len - 1];
   // largest possible observation for largest sample size
-  int max_obs = max_n * (max_n + 1) / 2;
+  int32_t max_obs = max_n * (max_n + 1) / 2;
 
   // list of results
   List out(len);
 
   // find zeros, ones and twos in sizes and store distributions
-  int pos_out = 0;
+  int32_t pos_out = 0;
   while(pos_out < len && n[pos_out] == 0) {
     out[pos_out] = NumericVector(1, 1.0);
     pos_out++;
@@ -32,10 +32,10 @@ List sign_rank_probs_int(
     dist[0] = dist[1] = 0.5;
 
     // compute non-trivial distributions
-    for(int k = 2; k <= max_n; k++) {
-      int end_new = k * (k + 1) / 2;
+    for(int32_t k = 2; k <= max_n; k++) {
+      int32_t end_new = k * (k + 1) / 2;
 
-      for(int j = end_new; j >= k; j--) {
+      for(int32_t j = end_new; j >= k; j--) {
         dist[j - k] /= 2;
         dist[j] += dist[j - k];
       }
